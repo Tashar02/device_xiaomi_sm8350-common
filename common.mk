ef8c9313b8381102890a9712dc1b2ccf1cc77379
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-03-17 22:31:55 +0600

    Revert "sm8350-common: Add bootctrl to PRODUCT_SOONG_NAMESPACES"
    This reverts commit 3cfd4dfd48948ec936945dc85e81edd72ab7e739.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- common.mk
+++ common.mk
@@ -455,7 +455,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/bootctrl \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
